81fedfd211762aedb8b9cddf1301555ce382b4b1
PrateekP <prateekpanta1789@gmail.com>
2025-04-19 22:17:08 +0530

    Added EventListener with event delegation to toggle navigation menu

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const body = document.querySelector("body");
 const toggleBall = document.querySelector(".toggle-ball");
 const menuToggle = document.querySelector(".menu-toggle");
+const primaryNavigation = document.querySelector(".primary-navigation");
 
 function setTheme() {
     body.classList.toggle("dark-mode");
@@ -10,5 +11,13 @@ function setTheme() {
 window.addEventListener("click", (e) => {
     if (e.target.closest(".theme-toggle")) {
         setTheme();
+    }
+
+    if (e.target.closest(".menu-toggle")) {
+        primaryNavigation.classList.toggle("active");
+    }
+
+    if (!e.target.matches(".primary-navigation") && !e.target.closest(".navbar")) {
+        primaryNavigation.classList.remove("active");
     }
 });
