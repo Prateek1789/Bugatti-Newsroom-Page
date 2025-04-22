daf237e4023178e74b077d390666ea5229df3667
PrateekP <prateekpanta1789@gmail.com>
2025-04-22 13:00:49 +0530

    Added btn functionality to close main nav menu

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const body = document.querySelector("body");
 const toggleBall = document.querySelector(".toggle-ball");
 const menuToggle = document.querySelector(".menu-toggle");
+const menuClose = document.querySelector(".menu-close-btn");
 const primaryNavigation = document.querySelector(".primary-navigation");
+
 
 function setTheme() {
     body.classList.toggle("dark-mode");
@@ -14,10 +16,20 @@ window.addEventListener("click", (e) => {
     }
 
     if (e.target.closest(".menu-toggle")) {
-        primaryNavigation.classList.toggle("active");
+        primaryNavigation.classList.add("active");
+        menuToggle.setAttribute("aria-expanded", "true");
     }
 
-    if (!e.target.matches(".primary-navigation") && !e.target.closest(".navbar")) {
+    if (e.target.closest(".menu-close-btn")) {
         primaryNavigation.classList.remove("active");
+        menuToggle.setAttribute("aria-expanded", "false");
+    }
+    
+    if (!e.target.matches(".primary-navigation") && !e.target.closest(".navbar")) {
+        // Close the menu if the user clicks outside of it
+        if (primaryNavigation.classList.contains("active")) {
+            primaryNavigation.classList.remove("active");
+            menuToggle.setAttribute("aria-expanded", "false");
+        }
     }
 });
