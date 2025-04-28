44e3a7b42847d724a6081ca7995341b510054fae
PrateekP <prateekpanta1789@gmail.com>
2025-04-28 13:55:32 +0530

    Optmised logic and added toggling for ARIA attributes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const body = document.querySelector("body");
+const subscribeBtn = document.querySelector(".btn-sub");
 const toggleBall = document.querySelector(".toggle-ball");
 const menuToggle = document.querySelector(".menu-toggle");
 const primaryNavigation = document.querySelector(".primary-navigation");
@@ -11,36 +12,43 @@ function setTheme() {
 };
 
 window.addEventListener("click", (e) => {
-    if (e.target.closest(".btn-sub")) {
+    let modalRect = modal.getBoundingClientRect();
+    let navMenuRect = primaryNavigation.getBoundingClientRect();
+
+    if (e.target == subscribeBtn) {
         modal.showModal();
+        subscribeBtn.setAttribute("aria-expanded", "true");
+        return;
     }
 
-    if (e.target.closest("#close-modal")) {
+    if (e.target.closest("#close-modal") || e.clientX < modalRect.left) {
         modal.close();
+        subscribeBtn.setAttribute("aria-expanded", "false");
+        return;
     }
 
     if (e.target.closest(".theme-toggle")) {
         setTheme();
+        let isPressed = e.target.getAttribute("aria-pressed") == "true";
+        e.target.setAttribute("aria-pressed", !isPressed ? "true" : "false");
+        return;
     }
 
     if (e.target.closest(".menu-toggle")) {
         primaryNavigation.classList.add("active");
+        primaryNavigation.querySelector(".menu-close-btn").focus();
         menuToggle.setAttribute("aria-expanded", "true");
         backdrop.classList.add("active");
+        return;
     }
 
-    if (e.target.closest(".menu-close-btn")) {
-        primaryNavigation.classList.remove("active");
-        menuToggle.setAttribute("aria-expanded", "false");
-        backdrop.classList.remove("active");
-    }
-    
-    if (!e.target.matches(".primary-navigation") && !e.target.closest(".navbar")) {
-        // Close the menu if the user clicks outside of it
+    if (e.target.closest(".menu-close-btn") || e.clientX < navMenuRect.left) {
         if (primaryNavigation.classList.contains("active")) {
             primaryNavigation.classList.remove("active");
             menuToggle.setAttribute("aria-expanded", "false");
             backdrop.classList.remove("active");
+            menuToggle.focus();
         }
+        return;
     }
 });
